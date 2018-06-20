fc9d8265fc7c1270e442f0c9e2b8bf7e3b72aa2f
PranotiT <33317679+PranotiT@users.noreply.github.com>
2018-06-20 08:52:49 +0200

    initial test file to check if works via Heroku

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Experiment</title>
+	
+</head>
+
+<body>
+	<div style="background-color:orange;color:black;border:1px solid black;width:200px;height:200px;font-size:18px;">Testing this !</div>
+
+
+</body>
+</html>
